80ed855767be5dc0abf0ddba9589ce0d36349b58
Mykel Dicus <mykeldicus@gmail.com>
2022-08-23 17:54:06 -0700

    psedocode for testimonials created

---
+++ js/testimonial.js
@@ -0,0 +1,5 @@
+//psuedocode//
+//if it is the first of the month
+    //get random testimonial from server
+//then insert testimonial into first testimonial window
+    //do this to the other two testimonial windows
